018d66d8c9f337b316a2f92a2d8e12412ab28b5c
Jon Jampen <jon.jampen@cryptography.ch>
2022-02-02 21:06:49 +0100

    Moving all includes to index.php.

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<?php
+include("app/database/connection.php");
+$page = "register";
+
+
+if(isset($_GET["page"])) {
+    $page = $_GET["page"];
+}
+
+
+
+if ($page == "users") {
+    include("app/controllers/users.php");
+}
+elseif ($page == "register") {
+    include("register.php");
+}
+elseif ($page == "dashboard") {
+    include("dashboard.php");
+}
