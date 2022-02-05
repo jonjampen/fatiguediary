09569a86ec21dcab0a401e15b59fb474aedfbc00
Jon Jampen <jon.jampen@cryptography.ch>
2022-02-05 09:07:05 +0100

    Using prepared statement to add new user to the database while preventing from sql-injection.

--- src/app/controllers/users.php
+++ src/app/controllers/users.php
@@ -1,11 +1,19 @@
 <?php
-
+//Setting variables to values from the form.
 $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 
-$sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
-mysqli_query($conn, $sql);
+//Preparing the statement
+$stmt = $conn->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
 
+//Binding the parameters to the statement
+$stmt->bind_param("sss", $name, $email, $password); //"sss" because name, email, password are three strings
+
+//Executing the statement
+$stmt->execute();
+
+
+//Redirecting
 header("location: index.php?page=dashboard");
