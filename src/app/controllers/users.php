52928a65a47bd7ed7db6d043eeeaddeea84120e4
Jon Jampen <jon.jampen@cryptography.ch>
2022-02-05 09:36:02 +0100

    Setting name in session.

--- src/app/controllers/users.php
+++ src/app/controllers/users.php
@@ -1,5 +1,6 @@
 <?php
-//Setting variables to values from the form.
+
+//Setting variables to values from the form
 $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
@@ -7,15 +8,27 @@ $password = $_POST['password'];
 //Hashing password
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-//Preparing the statement
-$stmt = $conn->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
 
-//Binding the parameters to the statement
-$stmt->bind_param("sss", $name, $email, $hashed_password); //"sss" because name, email, password are three strings
+//If registering
+if(isset($_POST['register'])) {
+    //Preparing the statement
+    $stmt = $conn->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
 
-//Executing the statement
-$stmt->execute();
+    //Binding the parameters to the statement
+    $stmt->bind_param("sss", $name, $email, $hashed_password); //"sss" because name, email, password are three strings
+
+    //Executing the statement
+    $stmt->execute();
+    login($_POST);
+}
 
 
-//Redirecting
-header("location: index.php?page=dashboard");
+
+function login($user) {
+    $_SESSION['name'] = $user['name'];
+    
+    //Redirecting
+    header("location: index.php?page=dashboard");
+}
+
+?>
