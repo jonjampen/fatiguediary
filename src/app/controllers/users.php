1fd200d5f8865e3bbb3df5b6a70cdc75fdf56ed1
Jon Jampen <jon.jampen@cryptography.ch>
2022-02-05 09:13:47 +0100

    Hashing password before adding the user to the db.

--- src/app/controllers/users.php
+++ src/app/controllers/users.php
@@ -4,12 +4,14 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
+//Hashing password
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
 //Preparing the statement
 $stmt = $conn->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
 
 //Binding the parameters to the statement
-$stmt->bind_param("sss", $name, $email, $password); //"sss" because name, email, password are three strings
+$stmt->bind_param("sss", $name, $email, $hashed_password); //"sss" because name, email, password are three strings
 
 //Executing the statement
 $stmt->execute();
