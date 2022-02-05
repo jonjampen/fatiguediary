d590026571a14faaa03444dde004b64da7f8007a
Jon Jampen <jon.jampen@cryptography.ch>
2022-02-05 08:49:06 +0100

    Adding values from registerform into the db using post.

--- src/app/controllers/users.php
+++ src/app/controllers/users.php
@@ -1,6 +1,11 @@
 <?php
 
-$sql = "INSERT INTO users (name, email, password) VALUES ('John Doe', 'john@doe.com', 'password123')";
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+$sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
 mysqli_query($conn, $sql);
 
 header("location: index.php?page=dashboard");
