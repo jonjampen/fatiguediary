16fc5185ed9b444f4a95f344867f8ffb86c1d184
Jon Jampen <jon.jampen@cryptography.ch>
2022-03-05 16:47:24 +0100

    Adding all the information from the form to the url and setting it as value.

--- src/addnew.php
+++ src/addnew.php
@@ -1,3 +1,17 @@
+<?php
+if (isset($_GET['energy'])) {
+    $date = $_GET['date'];
+    $time = $_GET['time'];
+    $energy = $_GET['energy'];
+    $notes = $_GET['notes'];
+} else {
+    $date = '';
+    $time = '';
+    $energy = 5;
+    $notes = '';
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,16 +38,16 @@
         <div class="add-form">
             <form action="index.php?page=add-new" method="post">
                 <div class="datetime">
-                    <div><i class="fa-solid fa-calendar-days"></i> <input type="date" name="date" id="currentDate"></div>
+                    <div><i class="fa-solid fa-calendar-days"></i> <input type="date" name="date" id="currentDate" <?php echo("value= '" . $date . "'"); ?>></div>
                 
-                    <div><i class="fa-solid fa-clock"></i> <input type="time" name="time" id="currentTime"></div>
+                    <div><i class="fa-solid fa-clock"></i> <input type="time" name="time" id="currentTime" <?php echo("value= '" . $time . "'"); ?>></div>
                 </div>
                 <div class="add-section">
                     <div class="title">
                         <h3>Energie-Level</h3>
                         <i class="fa-solid fa-circle-info"></i>
                     </div>
-                    <input class="slider" id="energySlider" type="range" name="energylevel" min="0" max="10" value="5" step="0.5">
+                    <input class="slider" id="energySlider" type="range" name="energylevel" min="0" max="10" <?php echo("value= '" . $energy . "'"); ?> step="0.5">
                     <div class="description">
                         <h1 id="energyValue" class="level-text"></h1>
                         <h1 id="energyIcon"></h1>
@@ -60,8 +74,7 @@
                         <a href="#" class="activity add"><i class="fa-solid fa-plus fa-2x"></i></a>
                     </div>
                 </div>
-                
-                <textarea name="notes" rows="6" placeholder="Notizen..."></textarea>
+                <textarea name="notes" id="notes" rows="6" placeholder="Notizen..."><?php echo($notes); ?></textarea>
         
                 <button type="submit" name="add-energy">Hinzufügen</button>
 
@@ -73,34 +86,46 @@
 
 <script src="assets/js/visualizeValue.js"></script>
 <script>
-    //calculate todays date and time
-    var today = new Date();
-    //consider timezone offset
-    today.setMinutes(today.getMinutes() - today.getTimezoneOffset());
-    
-    var date = today.toISOString().substring(0,10);
-    var time = today.toISOString().substring(11,16);
-    
-    document.getElementById("currentDate").value = date;
-    document.getElementById("currentTime").value = time;
+    <?php if (!isset($_GET['energy'])) : ?>
+        //calculate todays date and time
+        var today = new Date();
+        //consider timezone offset
+        today.setMinutes(today.getMinutes() - today.getTimezoneOffset());
+        var date = today.toISOString().substring(0,10);
+        var time = today.toISOString().substring(11,16);
+        document.getElementById("currentDate").value = date;
+        document.getElementById("currentTime").value = time;
+    <?php endif; ?>
 
     var energySlider = document.getElementById("energySlider");
     var energyValue = document.getElementById("energyValue");
     var energyIcon = document.getElementById("energyIcon");
-
-    //get value
+    var notes = document.getElementById("notes");
+    var date = document.getElementById("currentDate");
+    var time = document.getElementById("currentTime");
 
     //display first slider value, emoji and color
     energyValue.innerHTML = energySlider.value;
     calculateEmoji(energySlider.value, energyIcon);
     calculateColor(energySlider.value, energyValue);
 
-    //update slider
     energySlider.addEventListener('input', writeValue, false);
+    notes.addEventListener('input', addToUrl, false);
+    date.addEventListener('input', addToUrl, false);
+    time.addEventListener('input', addToUrl, false);
+    
+    //update slider
     function writeValue() {
         energyValue.innerHTML = energySlider.value;
         calculateEmoji(energySlider.value, energyIcon);
         calculateColor(energySlider.value, energyValue);
+        //add energylevel to url
+        addToUrl();
+    }
+
+    //add notes to url
+    function addToUrl() {
+        window.history.pushState({urlPath:'index.php'}, "", '?page=add-new&date=' + date.value + '&time=' + time.value + '&energy=' + energySlider.value + '&notes=' + notes.value);
     }
 </script>
 
