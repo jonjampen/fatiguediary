4395eb2a3f5aab84661b314f7ae1429c0d0c12b5
Jon Jampen <jon.jampen@cryptography.ch>
2022-04-07 18:01:46 +0200

    Changing title in login.php.

--- src/login.php
+++ src/login.php
@@ -12,7 +12,7 @@ if (isset($_GET['email'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
+    <title>Anmelden</title>
 </head>
 <body>
     <div class="container">
