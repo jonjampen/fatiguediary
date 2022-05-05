3619ca6c2e097c6449be42335d28194c45908d2f
Jon Jampen <jon.jampen@cryptography.ch>
2022-05-05 21:15:18 +0200

    Changing spacing.

--- src/app/controllers/mail.php
+++ src/app/controllers/mail.php
@@ -2,7 +2,7 @@
 if(isset($_POST['send-contact-mail'])){
     $to = "info@fatiguediary.ch";
     $subject = $_POST['subject'];
-    $message = $_POST['name'] . "schrieb folgendes:" . "\n\n" . $_POST['message'];
+    $message = $_POST['name'] . "schrieb folgendes: " . "\n\n" . $_POST['message'];
 
     $headers = "MIME-Version: 1.0" . "\n";
     $headers .= "Content-type:text/plain;charset=UTF-8" . "\n";
@@ -11,7 +11,7 @@ if(isset($_POST['send-contact-mail'])){
 
     mail($to,$subject,$message,$headers);
 
-    $conf_message =  "Vielen Dank für Ihre Nachricht, wir melden und in Kürze." . "\n" . "Kopie der Nachricht:" . "\n\n" . $_POST['message'] . "\n\n" . "Bei weiteren Anmerkungen antworten Sie bitte auf diese E-Mail.";
+    $conf_message =  "Vielen Dank für Ihre Nachricht, wir melden und in Kürze." . "\n" . "Kopie der Nachricht:" . "\n\n" . $_POST['message'] . "\n\n\n" . "Bei weiteren Anmerkungen antworten Sie bitte auf diese E-Mail.";
     sendConfirmationMail($_POST['email'], "info@fatiguediary.ch", "Ihre Nachricht wurde versendet", $conf_message);
 }
 
