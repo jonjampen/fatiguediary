59a2fe011a60e4e464ac2279d0a25b2194ced868
Jon Jampen <jon.jampen@cryptography.ch>
2022-05-15 17:52:10 +0200

    Removing items from bottom-nav.

--- src/app/includes/bottomNavbar.php
+++ src/app/includes/bottomNavbar.php
@@ -1,9 +1,7 @@
 <nav class="bottom-nav">
     <ul class="nav_items">
         <li><a href="index.php?page=dashboard"><span class="material-icons">bar_chart</span></a></li>
-        <li><a href="index.php?page=entries"><span class="material-icons">assignment</span></a></li>
         <li><a href="index.php?page=add-new" class="add"><span class="material-icons">add</span></a></li>
-        <li><a href="index.php?page=tricks"><span class="material-icons">lightbulb</span></a></li>
-        <li><a href="index.php?page=profile"><span class="material-icons">person</span></a></li>
+        <li><a href="index.php?page=entries"><span class="material-icons">assignment</span></a></li>
     </ul>
 </nav>
