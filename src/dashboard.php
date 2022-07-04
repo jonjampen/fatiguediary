98e4aeea1271e61daf15c38555ff43f9e5afe08e
Jon Jampen <jon.jampen@cryptography.ch>
2022-07-04 08:26:05 +0200

    Changing chart class.

--- src/dashboard.php
+++ src/dashboard.php
@@ -13,70 +13,70 @@ $energylevels = getEnergyLevelsByDate($todayDate);
     <h3 class="welcome-text">Willkommen <?php echo($_SESSION['name']);?></h3>
     
     <div class="container diagram">
-      <div id="chart"></div>
-	  </div>
+      <div id="energylevel_area"></div>
+    </div>
 
-    
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    
     <script>
-    var options = {
-          series: [{
-          name: 'series1',
-          data: [
-            <?php
-            foreach ($energylevels as $energylevel) {
-              echo($energylevel['energylevel'] . ',');
-            };
-            ?>
-          ]
-        }],
-          chart: {
-          height: 290,
-          type: 'area'
-        },
-        dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          curve: 'smooth'
-        },
-        colors:['#F55B53'],
-        xaxis: {
-          type: 'datetime',
-          categories: [
-            <?php
-            foreach ($energylevels as $energylevel) {
-              echo('"' . date("Y-m-d H:i:s", strtotime($energylevel['datetime'])) . '"' . ',');
-            };
-            ?>
-          ],
-          labels: {
-            style: {
-              colors: '#FFFFFF',
-            }
-          },
-          min: new Date('12 Jun 2022 06:00:00'),
-          max: new Date('12 Jun 2022 22:30:00'),
-        },
-        yaxis: {
-          labels: {
-            style: {
-              colors: '#FFFFFF',
-            }
-          },
-          min: 0,
-          max: 10
-        },
-        tooltip: {
-          x: {
-            format: 'dd/MM/yy HH:mm'
-          },
-        },
+        var options = {
+            series: [{
+            name: 'series1',
+            data: [
+                <?php
+                foreach ($energylevels as $energylevel) {
+                echo($energylevel['energylevel'] . ',');
+                };
+                ?>
+            ]
+            }],
+            chart: {
+            height: 290,
+            type: 'area'
+            },
+            dataLabels: {
+            enabled: false
+            },
+            stroke: {
+            curve: 'smooth'
+            },
+            colors:['#F55B53'],
+            xaxis: {
+            type: 'datetime',
+            categories: [
+                <?php
+                foreach ($energylevels as $energylevel) {
+                echo('"' . date("Y-m-d H:i:s", strtotime($energylevel['datetime'])) . '"' . ',');
+                };
+                ?>
+            ],
+            labels: {
+                style: {
+                colors: '#FFFFFF',
+                }
+            },
+            min: new Date('12 Jun 2022 06:00:00'),
+            max: new Date('12 Jun 2022 22:30:00'),
+            },
+            yaxis: {
+            labels: {
+                style: {
+                colors: '#FFFFFF',
+                }
+            },
+            min: 0,
+            max: 10
+            },
+            tooltip: {
+            x: {
+                format: 'dd/MM/yy HH:mm'
+            },
+            },
         };
 
         var chart = new ApexCharts(document.querySelector("#chart"), options);
         chart.render();
-</script>
+    </script>
 
 </body>
 </html>
