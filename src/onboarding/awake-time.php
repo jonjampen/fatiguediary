0b10f9061e65be424a492b374a2b04f77ab0b688
Jon Jampen <jon.jampen@cryptography.ch>
2022-07-13 22:35:37 +0200

    Changing text in onboarding.

--- src/onboarding/awake-time.php
+++ src/onboarding/awake-time.php
@@ -7,13 +7,13 @@ includeToastify();
     <div class="onboarding-screen">
         <h2>Tageszeit</h2>
         <img src="assets/img/onboarding-awake.svg" alt="">
-        <p>Wann bist du wach?</p>
+        <p>Wann bist du nomalerweise wach?</p>
         <div class="btn-option">
             <input type="time" name="" id="wake_up" value="06:00">
             -
             <input type="time" name="" id="bed" value="22:30">
         </div>
-        <p>Dies Information wird für die bessere Darstellung der Diagramme verwendet.</p>
+        <p>Diese Information wird für die bessere Darstellung der Diagramme verwendet.</p>
         <div class="btn-option">
             <button class="btn-secondary" id="btn_next">Weiter</button>
         </div>
--- src/onboarding/tutorial.php
+++ src/onboarding/tutorial.php
@@ -7,7 +7,7 @@ includeToastify();
     <div class="onboarding-screen">
         <h2>Tutorial</h2>
         <img src="assets/img/onboarding-tutorial.svg" alt="">
-        <p>Falls du nach dem kurzen Tutorial noch Hilfe brauchst, gehe im Seiten-Menü auf "Hilfe"</p>
+        <p>Du kannst diese Tour jederzeit wiederholen, gehe dazu einfach im Seiten-Menu auf "Tour starten".</p>
         <form action="<?php echo("index.php?page=set-settings&t=" . $_GET['t'] . "&w=" . $_GET['w'] . "&b=" . $_GET['b'] . "&n=" . $_GET['n']); ?>" method="post">
             <div class="btn-option">
                 <button class="btn-secondary" id="btn_next" type="submit" name="set-settings">Start</button>
