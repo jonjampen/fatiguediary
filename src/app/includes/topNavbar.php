8458161375114dede7fe1459dfd962a679bae2d7
Jon Jampen <jon.jampen@cryptography.ch>
2022-08-28 12:10:02 +0200

    Correct link to start tour in correct language.

--- src/app/includes/topNavbar.php
+++ src/app/includes/topNavbar.php
@@ -14,7 +14,7 @@
         <li><a href="index.php?page=app">Fatigue Diary</a></li>
         <li><a href="index.php?page=new"><?php echo $text['whats-new']; ?></a></li>
         <li><a href="index.php?page=contact"><?php echo $text['contact']; ?></a></li>
-        <li><a href="intro/Dashboard.php"><?php echo $text['start-tutorial']; ?></a></li>
+        <li><a href="intro/<?php echo $_SESSION['settings']['language']; ?>/Dashboard.php"><?php echo $text['start-tutorial']; ?></a></li>
         <li><a href="index.php?page=logout" class="logout"><i class="fa-solid fa-arrow-right-from-bracket"></i> <?php echo $text['logout']; ?></a></li>
     </ul>
 </div>
