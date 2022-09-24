f76be644f703f752b4a26d29495aa1a07b81c3f9
Jon Jampen <jon.jampen@cryptography.ch>
2022-09-24 20:09:24 +0200

    Adding app.js file for pwa.

---
+++ src/app.js
@@ -0,0 +1,6 @@
+// register service worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+    .then((reg) => console.log('service worker registered', reg))
+    .catch((error) => console.log('error', error));
+}
