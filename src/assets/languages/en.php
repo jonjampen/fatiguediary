e89cbac9f99e01b1e24f6278c64fb7cc91b6d417
Jon Jampen <jon.jampen@cryptography.ch>
2022-09-25 11:53:10 +0200

    Adding how to and installation pages.

--- src/assets/languages/en.php
+++ src/assets/languages/en.php
@@ -99,6 +99,7 @@ $text =
     'content-reset-pw' => 'Click on the link to reset your password: ',
 
     'whats-new' => 'What\'s new?',
+    'how-it-works' => 'How it works',
     'contact' => 'Contact',
     'logout' => 'Logout',
     'start-tutorial' => 'Start tutorial',
@@ -137,4 +138,15 @@ $text =
     'about-title' => 'Developed by a fatigue patient',
     'about-desc' => 'Hi, I\'m Jon Jampen. I am suffering from chronic fatigue myself and I have built this web app to show others what is helping me. This app is my matura project.',
     'copy' => 'All rights reserved. Designed and developed by Jon Jampen.',
+
+
+    'how-fd-text' => 'Fatigue Diary works in your Browser or can be installed <a href="index.php?page=install">here</a>. A detailed tutorial on how to use Fatigue Diary is available <a href="intro/' . $_SESSION['settings']['language'] . '/Dashboard.php">here</a>. <br> Fatigue Diary can be used as a diary for patients with Fatigue and can help them with pacing. <br> Enter your energy level after every activity and the app will create a chart that displays your energy throughout the day. Other charts help you to compare days or months with each other. <br> This app was created by Jon Jampen - a fatigue patient - as a project for school.',
+    'how-avg-title' => 'Daily Average',
+    'how-avg-text' => 'The daily average is not the arithmetic mean of all entries but it is calculated with respect to time. Two entries have the same influence on the daily average as many entries, as long as they have the same time difference. <br>In the following example on both days the energy level starts at 10 and ends at 5. The difference is that the energy level in the left chart goes down to 5 earlier than in the chart on the right. The arithmetic mean would be the same for both (6.7) but clearly the day on the right is better than the one on the left. That\'s the reason why time has to be included in the calculation.',
+    'how-activities-title' => 'Calculate activities',
+    'how-activities-text' => 'The energy boosting and draining activities are calculated using the data form the past 30 days. Only the direct impact of the activity on your energy level is taken into account. <br> Activities, that on average have a positive influence are displayed in descending order under the activities that boost your energy. Every activity has one of three colors. The best activity always has a green color, whereas the one with the least positive impact is yellow. The other activities are colored with one of the three colors depending on their impact. <br> The same follows for the energy draining activities.',
+
+
+    'install-fd' => 'Install Fatigue Diary',
+    'install-text' => 'You will soon be able to install Fatigue Diary.',
 ];
