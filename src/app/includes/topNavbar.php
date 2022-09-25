4ed22b6f7e16a1ac2568aef5c2f61f64600de54d
Jon Jampen <jon.jampen@cryptography.ch>
2022-09-25 17:31:30 +0200

    Adding install button if not already in standalone view.

--- src/app/includes/topNavbar.php
+++ src/app/includes/topNavbar.php
@@ -17,6 +17,7 @@
         <li><a href="index.php?page=contact"><?php echo $text['contact']; ?></a></li>
         <li><a href="index.php?page=how"><?php echo $text['how-it-works']; ?></a></li>
         <li><a href="intro/<?php echo $_SESSION['settings']['language']; ?>/Dashboard.php"><?php echo $text['start-tutorial']; ?></a></li>
+        <button class="btn-primary" name="add-activity" id="installApp" onclick="installPWA()"><?php echo $text['install']; ?></button>
         <?php if(isset($_SESSION['id'])): ?>
             <li><a href="index.php?page=logout" class="logout"><i class="fa-solid fa-arrow-right-from-bracket"></i> <?php echo $text['logout']; ?></a></li>
         <?php else: ?>
@@ -38,5 +39,9 @@ function closeNav() {
     document.getElementById("sidenav").style.display = "none";
     document.getElementById("open").style.display = "inline";
     document.getElementById("close").style.display = "none";
+}
+
+if ((window.matchMedia('(display-mode: standalone)').matches)) {
+    document.getElementById("installApp").classList.add("hidden");
 }
 </script>
