f1dc01dc2eb7d05fe8f485d59ca63a1b7e5b633f
Jon Jampen <jon.jampen@cryptography.ch>
2022-10-01 10:34:20 +0200

    Removing console log from serviceworker.

--- src/sw.js
+++ src/sw.js
@@ -1,14 +1,14 @@
 // install service worker
 self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
+    // console.log('[Service Worker] Install');
 });
 
 // activate service worker
 self.addEventListener('activate', (e) => {
-  console.log("service worker activated");
+  // console.log("service worker activated");
 })
 
 // fetch event
 self.addEventListener('fetch', (e) => {
-  console.log('fetch event', e);
+  // console.log('fetch event', e);
 })
