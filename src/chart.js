c14c59910c1db9bf083ee838befd55c5010e7cc3
Jon Jampen <jon.jampen@cryptography.ch>
2022-10-01 10:33:31 +0200

    Removing chart.js file.

--- src/chart.js
+++
@@ -1,8 +0,0 @@
-var defaultOptions = {
-    noData: {
-        text: 'Loading...'
-    }
-};
-
-var energylevel_area = new ApexCharts(document.querySelector("#energylevel_area"), defaultOptions);
-energylevel_area.render();
