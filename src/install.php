dead57cbfc121bf02a6e0c7530e62fa4d3ea5561
Jon Jampen <jon.jampen@cryptography.ch>
2022-10-16 20:06:07 +0200

    adding in window.

--- src/install.php
+++ src/install.php
@@ -7,7 +7,9 @@ includeToastify();
 <div class="install-screen" style="text-align: justify;">
     <h2><?php echo $text['install-fd']; ?></h2>
     <p id="install-tutorial"></p>
-    
+    <!-- <p><button class="btn-primary" style="width:auto" name="add-activity" id="installApp" onclick="installPWA(true);">Chrome/Edge/Opera Install</button></p> -->
+    <br>
+    <p>Safari/Firefox: Klicke auf die drei Punkte unten rechts und klicke auf Installieren.</p>
     <!-- <p><?php //echo $text['install-text']; ?></p> -->
 
 </div>
@@ -33,7 +35,8 @@ includeToastify();
     })();
 
 
-    if (isEventSupported('BeforeInstallPromptEvent')) {
+    // if (isEventSupported('BeforeInstallPromptEvent')) {
+    if ("beforeinstallprompt" in window) {
         tutorial = '<button class="btn-primary" name="add-activity" id="installApp" onclick="installPWA(true);"><?php echo $text["install"]; ?></button>';
     }
     else {
