b1a058244bce6d1de3abe0483597f5530e61a4ae
Jon Jampen <jon.jampen@cryptography.ch>
2022-10-16 19:54:12 +0200

    Checking if browser supports event beforeinstallprompt.

--- src/install.php
+++ src/install.php
@@ -6,7 +6,39 @@ includeToastify();
 
 <div class="install-screen" style="text-align: justify;">
     <h2><?php echo $text['install-fd']; ?></h2>
-    <button class="btn-primary" name="add-activity" id="installApp" onclick="installPWA(true);"><?php echo $text['install']; ?></button>
-    <p><?php echo $text['install-text']; ?></p>
+    <p id="install-tutorial"></p>
+    
+    <!-- <p><?php //echo $text['install-text']; ?></p> -->
 
 </div>
+<script>
+    var isEventSupported = (function(){
+    var TAGNAMES = {
+        'select':'input','change':'input',
+        'submit':'form','reset':'form',
+        'error':'img','load':'img','abort':'img'
+    }
+    function isEventSupported(eventName) {
+        var el = document.createElement(TAGNAMES[eventName] || 'div');
+        eventName = 'on' + eventName;
+        var isSupported = (eventName in el);
+        if (!isSupported) {
+            el.setAttribute(eventName, 'return;');
+            isSupported = typeof el[eventName] == 'function';
+        }
+        el = null;
+        return isSupported;
+    }
+    return isEventSupported;
+    })();
+
+
+    if (isEventSupported('BeforeInstallPromptEvent')) {
+        tutorial = '<button class="btn-primary" name="add-activity" id="installApp" onclick="installPWA(true);"><?php echo $text["install"]; ?></button>';
+    }
+    else {
+        tutorial = 'Klicke auf die drei Punkte unten rechts und klicke auf Installieren.';
+    }
+
+    document.getElementById('install-tutorial').innerHTML = tutorial;
+</script>
