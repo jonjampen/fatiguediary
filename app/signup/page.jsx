a77861dfce84d8cf1cfaface41cc02744583c45e
Jon Jampen <jon.jampen@cryptography.ch>
2023-08-15 16:44:27 +0200

    Adjusting login screen and adding signup screen.*

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -17,12 +17,16 @@ export default function Login() {
         <main className="h-[100vh] flex justify-center items-center">
             <Card className="w-[350px]">
                 <CardHeader className="flex-col items-center">
-                    <CardTitle>Login</CardTitle>
-                    <CardDescription>Enter your email and password to continue</CardDescription>
+                    <CardTitle>Signup</CardTitle>
+                    <CardDescription>Enter your information to create an account</CardDescription>
                 </CardHeader>
                 <CardContent>
                     <form>
                         <div className="grid w-full items-center gap-4">
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="name">Name</Label>
+                                <Input type="name" placeholder="Name" />
+                            </div>
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="name">Email</Label>
                                 <Input type="email" placeholder="Email" />
@@ -31,12 +35,16 @@ export default function Login() {
                                 <Label htmlFor="framework">Password</Label>
                                 <Input type="password" placeholder="Password" />
                             </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="framework">Password Confirmation</Label>
+                                <Input type="password" placeholder="Repeat your password" />
+                            </div>
                         </div>
                     </form>
                 </CardContent>
                 <CardFooter className="flex-col items-center">
-                    <Button className="w-full">Login</Button>
-                    <CardDescription className="mt-2">Don't have an account yet? <a href="/signup">Signup now</a></CardDescription>
+                    <Button className="w-full">Signup</Button>
+                    <CardDescription className="mt-2">Already have an account? <a href="/login">Login</a></CardDescription>
                 </CardFooter>
             </Card>
         </main>
