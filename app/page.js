793526ef5b7b0a883f3b3104a0882deba057b08f
Jon Jampen <jon.jampen@cryptography.ch>
2023-08-15 16:10:55 +0200

    Adding shadcn and button.

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
@@ -108,6 +109,7 @@ export default function Home() {
           </p>
         </a>
       </div>
+      <Button>Button</Button>
     </main>
   )
 }
