e183f95bb7aab6aafb2df0af4b11f36e4626bc9e
Jon Jampen <jon.jampen@cryptography.ch>
2023-08-16 13:53:55 +0200

    Own component for date picker.

--- app/user/dashboard/page.jsx
+++ app/user/dashboard/page.jsx
@@ -1,52 +1,10 @@
-"use client"
+import DatePicker from '@/components/DatePicker'
+import React from 'react'
 
-import * as React from "react"
-import { format } from "date-fns"
-import { Calendar as CalendarIcon } from "lucide-react"
-
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
-import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@/components/ui/popover"
-import { ChevronLeft, ChevronRight } from "lucide-react"
-
-export default function DatePicker() {
-    const [date, setDate] = React.useState()
-
+export default function Dashboard() {
     return (
-        <div className="flex gap-2">
-            <Button variant="outline" size="icon" className="" >
-                <ChevronLeft className="h-4 w-4" />
-            </Button>
-            <Popover>
-                <PopoverTrigger asChild>
-                    <Button
-                        variant={"outline"}
-                        className={cn(
-                            "w-[280px] justify-start text-left font-normal",
-                            !date && "text-muted-foreground"
-                        )}
-                    >
-                        <CalendarIcon className="mr-2 h-4 w-4" />
-                        {date ? format(date, "PPP") : <span>Pick a starting date</span>}
-                    </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0">
-                    <Calendar
-                        mode="single"
-                        selected={date}
-                        onSelect={setDate}
-                        initialFocus
-                    />
-                </PopoverContent>
-            </Popover>
-            <Button variant="outline" size="icon" className="" >
-                <ChevronRight className="h-4 w-4" />
-            </Button>
+        <div className="w-full flex items-start justify-center">
+            <DatePicker />
         </div>
     )
 }
