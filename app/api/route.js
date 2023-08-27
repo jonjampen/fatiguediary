cb7e6d5dd714ab3d04418ea9697dab12e9114f82
Jon Jampen <jon.jampen@cryptography.ch>
2023-08-27 09:28:46 +0200

    Making request to api.

---
+++ app/api/route.js
@@ -0,0 +1,6 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+    // Do whatever you want
+    return NextResponse.json({ message: "Hello World" }, { status: 200 });
+}
--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,9 @@ import Image from 'next/image'
 import { Button } from "@/components/ui/button"
 
 export default function Home() {
+
+  fetch(process.env.URL + "/api").then(response => response.json()).then(data => console.log(data))
+
   return (
     <main>
       <Button>Button</Button>
