2c5b24e09e137d12325188d87dd7e3ce39da47c8
Jon Jampen <jon.jampen@cryptography.ch>
2023-09-15 10:24:28 +0200

    Cleaning code.

--- app/api/route.js
+++ app/api/route.js
@@ -23,10 +23,6 @@ export async function POST(request) {
     if (session) {
         userid = session.user.id;
     }
-    console.log(type)
-    console.log(session)
-    console.log(request)
-    console.log("---")
 
     let rows;
     let query = '';
