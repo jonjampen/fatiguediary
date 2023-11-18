ae7e22327a7f747283ae69ea5e9d3cc9a5962813
Jon Jampen <jon.jampen@cryptography.ch>
2023-11-18 18:22:42 +0100

    Generalisation of path, only change node version.

--- _service/start.sh
+++ _service/start.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-HOME="/home/fatiguediary-beta"
-NODE="${HOME}/.nvm/versions/node/v18.18.2"
-export PATH=${NODE}/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
+NODE_VERSION="v18.18.2"
+
+HOME="/home/${USER}"
+NODE_DIR="${HOME}/.nvm/versions/node/${NODE_VERSION}"
+
+export PATH=${NODE_DIR}/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
 export NVM_DIR=${HOME}/.nvm
-export NVM_INC=${NODE}/include/node
-export NVM_BIN=${NODE}/bin
+export NVM_INC=${NODE_DIR}/include/node
+export NVM_BIN=${NODE_DIR}/bin
 export NVM_CD_FLAGS=
 
 cd ~/fatiguediary
