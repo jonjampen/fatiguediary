4948accb01def78fcf6470ae4f15f531d891594a
Jon Jampen <jon.jampen@cryptography.ch>
2023-11-20 11:28:42 +0100

    Updating install.sh for auto deployment.

--- _service/install.sh
+++ _service/install.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+NODE_VERSION="v18.18.2"
 GIT_REPO="fatiguediary"
 
 DIR="$(cd -- "$(dirname "${0}")" > /dev/null 2>&1 ; pwd -P)"
@@ -12,15 +13,28 @@ DIR=${DIR%"${SUFFIX}"}
 
 HOME="/home/${DIR}"
 SERVICE="${DIR}"
+NODE_DIR="${HOME}/.nvm/versions/node/${NODE_VERSION}"
 
+# refresh repo
+cd ${HOME}/${GIT_REPO}
+git checkout -f HEAD
+git pull --rebase
+
+# build
+cd ${HOME}/${GIT_REPO}
+${NODE_DIR}/bin/npm install --production
+${NODE_DIR}/bin/npm run build
+
+# install service
 cp -f ${HOME}/${GIT_REPO}/_service/start.sh ${HOME}/
 cp -f ${HOME}/${GIT_REPO}/_service/template.service /etc/systemd/system/${SERVICE}.service
-
 sed -i -e "s/SERVICE/${SERVICE}/" /etc/systemd/system/${SERVICE}.service
-
-systemctl stop ${SERVICE}
 systemctl daemon-reload
 systemctl enable ${SERVICE}
+
+# restart service
+systemctl stop ${SERVICE}
+sleep 1
 systemctl start ${SERVICE}
 sleep 1
 systemctl status ${SERVICE}
