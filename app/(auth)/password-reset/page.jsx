5908c01863eac465df2ad3aa7a3c96203a87b6dc
Jon Jampen <jon.jampen@cryptography.ch>
2024-01-02 19:28:25 +0100

    Adding error handling to password reset.

--- app/(auth)/password-reset/page.jsx
+++ app/(auth)/password-reset/page.jsx
@@ -24,17 +24,27 @@ export default function Login({ searchParams }) {
         },
     ]
 
-    if (typeof searchParams.success == "undefined") {
-        return (
-            <Form title="Request Password Reset" description="Enter your email and check your inbox." fields={fields} info="You will receive an email with a reset link." link="" linkText="" />
-        )
-    }
-    else {
+    let success = searchParams.success;
+
+    if (success === "true") {
         return (
             <div className="flex flex-col items-center">
                 <h1>Password Reset Request</h1>
                 <p>An email has been set to your inbox. Please click on the link in the email to change your password.</p>
             </div>
+        )
+    }
+    else if (success === "false") {
+        return (
+            <div className="flex flex-col items-center">
+                <h1>Error!</h1>
+                <p>The password reset email could not be sent! Please try again or contact support.</p>
+            </div>
+        )
+    }
+    else {
+        return (
+            <Form title="Request Password Reset" description="Enter your email and check your inbox." fields={fields} info="You will receive an email with a reset link." link="" linkText="" />
         )
     }
 }
