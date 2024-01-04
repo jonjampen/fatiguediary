dd7155c45264b79c8650e45d110711790280c788
Jon Jampen <jon.jampen@cryptography.ch>
2024-01-04 14:25:32 +0100

    adding a global error page.

---
+++ app/global-error.js
@@ -0,0 +1,27 @@
+'use client'
+
+import { useEffect } from 'react'
+
+export default function GlobalError({ error, reset }) {
+    useEffect(() => {
+        console.error(error)
+    }, [error])
+
+    return (
+        <html>
+            <body>
+                <div>
+                    <h2>Sorry, something went wrong! Click "Try again", reload the page, or contact support (info@fatiguediary.ch).</h2>
+                    <button className="border rounded bg-gray-100 text-black h-10 min-w-16"
+                        onClick={
+                            // Attempt to recover by trying to re-render the segment
+                            () => reset()
+                        }
+                    >
+                        Try again
+                    </button>
+                </div>
+            </body>
+        </html>
+    )
+}
