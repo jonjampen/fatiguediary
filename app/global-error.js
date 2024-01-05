cf2db935ef0c0812e2b0535d1822cd505f05fbb6
Jon Jampen <jon.jampen@cryptography.ch>
2024-01-05 11:55:40 +0100

    Replacing quote with &quot;

--- app/global-error.js
+++ app/global-error.js
@@ -11,7 +11,7 @@ export default function GlobalError({ error, reset }) {
         <html>
             <body>
                 <div>
-                    <h2>Sorry, something went wrong! Click "Try again", reload the page, or contact support (info@fatiguediary.ch).</h2>
+                    <h2>Sorry, something went wrong! Click &quot;Try again&quot;, reload the page, or contact support (info@fatiguediary.ch).</h2>
                     <button className="border rounded bg-gray-100 text-black h-10 min-w-16"
                         onClick={
                             // Attempt to recover by trying to re-render the segment
