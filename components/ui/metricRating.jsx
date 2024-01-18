3494792b3de007a5e6ea4afe4849771d6f46e396
Jon Jampen <jon.jampen@cryptography.ch>
2024-01-18 14:18:07 +0100

    Adding plus and minus buttons for number input.

--- components/ui/metricRating.jsx
+++ components/ui/metricRating.jsx
@@ -2,17 +2,25 @@
 
 import React, { useState } from 'react'
 import { Input } from './input';
+import { Button } from './button';
+import { Minus, Plus } from 'lucide-react';
 
 export default function MetricRating({ ratingType, selectedRating, setSelectedRating }) {
     const [value, setValue] = useState(selectedRating)
     function changeRating(e, val) {
+        console.log(val)
         e.preventDefault();
         setSelectedRating(val)
     }
 
     if (ratingType === "numberInput") {
         return (
-            <Input type="number" value={selectedRating} onChange={(e) => changeRating(e, e.target.value)} className="max-w-[152px] h-8" />
+            <div className='flex gap-2 w-[152px]'>
+                <Button variant="outline" size="icon" className="h-8 w-8 flex-shrink-0" onClick={(e) => changeRating(e, selectedRating - 1)}><Minus className="w-4 h-4" /></Button>
+                <Input type="number" value={selectedRating} onChange={(e) => changeRating(e, e.target.value)} className="h-8 flex-grow" />
+                <Button variant="outline" size="icon" className="h-8 w-8 flex-shrink-0" onClick={(e) => changeRating(e, selectedRating + 1)}><Plus className="w-4 h-4" /></Button>
+            </div>
+
         )
     }
     else {
